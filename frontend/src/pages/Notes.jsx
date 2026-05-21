011bddd8326b58de34707103e287a9e86b446081
Joseph Cardoza <80924062+JosephCardoza03@users.noreply.github.com>
2026-05-21 16:20:59 -0500

    Delete frontend directory

--- frontend/src/pages/Notes.jsx
+++
@@ -1,9 +0,0 @@
-import "../css/About.css"
-
-function Notes(){
-    return <div className="notes">
-        <h2>No About Us Yet</h2>
-    </div>
-}
-
-export default Notes;
--- frontend/src/pages/Schedule.jsx
+++
@@ -1,9 +0,0 @@
-import "../css/About.css"
-
-function Schedule(){
-    return <div className="schedule">
-        <h2>No About Us Yet</h2>
-    </div>
-}
-
-export default Schedule;
