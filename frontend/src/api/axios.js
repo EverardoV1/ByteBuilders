b56b5407fd6ba574869afb76fa49bfa72f2d00c2
Joseph Cardoza <80924062+JosephCardoza03@users.noreply.github.com>
2026-05-21 17:12:47 -0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://bytebuilders-app.onrender.com",
+    baseURL: "https://bytebuilders-app.onrender.com/auth",
     headers: {
         "Content-Type": "application/json"
     }
