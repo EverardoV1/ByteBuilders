f57323ec24d6a8adc8f56c3d74ea65d73eafd5c5
Destrea <JarodMMoore@gmail.com>
2026-05-21 18:10:17 -0500

    Fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,7 @@ const allowedOrigins = process.env.FRONTEND_ORIGIN
 const app = express()
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://becausewecaretx.com/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
